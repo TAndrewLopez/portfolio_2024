0ca8940f7248432e8d931763efb680c6b32dbb4d
Andrew Dobson <tandrewlopez@gmail.com>
2023-12-17 10:20:14 -0500

    new read me and removed archived project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-Photo by <a href="https://unsplash.com/@nathananderson?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Nathan Anderson</a> on <a href="https://unsplash.com/photos/grayscale-photography-of-mountains-and-trees-IghZxJgO94M?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+New portfolio for 2024.
+
+Background Photo by <a href="https://unsplash.com/@nathananderson?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Nathan Anderson</a> on <a href="https://unsplash.com/photos/grayscale-photography-of-mountains-and-trees-IghZxJgO94M?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
   
--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -17,7 +17,7 @@ export const PROJECT_DATA: Record<string, Project> = {
         liveURL: 'https://booking-tattoos.vercel.app/',
         imageURL: '/image/projects/tattoo.png',
         videoURL: '/video/tattoo.mp4',
-        description: "Designed and developed a web application that streamlined the workflow for a client. Deliverables include, client side UI with intake form, essential CRM operations to allow custom responses, creating, updating and assigning calendar events."
+        description: "Designed and developed a web application that streamlined the client's workflow from multiple platforms to a single location. Created a custom CRM for administrators to access their data and review, manage, and schedule appointments. Developed a custom calendar UI for the team to manage their consultations and appointments as well as add new custom calendar events."
     },
     records: {
         name: 'Gen. Records',
@@ -25,7 +25,7 @@ export const PROJECT_DATA: Record<string, Project> = {
         githubURL: 'https://github.com/TAndrewLopez/gen_records_nextjs',
         liveURL: 'https://gen-records.vercel.app/',
         imageURL: '/image/projects/records.png',
-        description: "Developed a functional E-Commerce website that fetches album information from Spotify's API to represent the product sold. Equipped with Stripe integration for secure payments, Redux for global state management, a custom CMS to allow administrators to easily manage product and inventory levels, and user authentication with specific role privileges.",
+        description: "Designed and developed a functional e-commerce website that fetches album information from Spotify's API to represent the product sold. Equipped with Stripe integration for secure payments, Redux for global state management, a custom CMS to allow administrators to easily manage product and inventory levels, and user authentication with specific role privileges.",
         images: ['/image/projects/records_splash.png', '/image/projects/records_shop.png',
             '/image/projects/records_album.png', '/image/projects/records_login.png',]
     },
@@ -36,7 +36,7 @@ export const PROJECT_DATA: Record<string, Project> = {
         liveURL: 'https://linkuistix.com/login',
         imageURL: '/image/projects/linkuistix.png',
         videoURL: '/video/linkuistixVideo.mp4',
-        description: "Collaborated on a word association game powered by a pre-trained AI model from TensorFlow. Led game development which was designed using core Object Oriented Programming principles. (ie: A.P.I.E.)"
+        description: "Collaborated on a word association game powered by a pre-trained AI model from TensorFlow. Led game development which was designed using core Object Oriented Programming principles."
     },
     dashboard: {
         name: 'FinTech Dashboard',
@@ -45,16 +45,16 @@ export const PROJECT_DATA: Record<string, Project> = {
         liveURL: '',
         imageURL: '/image/projects/dashboard.png',
         // githubURL: 'https://github.com/TAndrewLopez/FinTech-Dashboard-Client',
-        description: "This web application was designed to help users track the finances of their company. It displays key performance indicators (KPIs) and other data information using multiple charts and tables, such as line, pie, and bar charts.",
+        description: "Developed a web application to help users track the finances of their company. It displays key performance indicators (KPIs) and other data information using multiple charts and tables, such as line, pie, and bar charts.",
         images: ['/image/projects/dashboard_splash.png']
     },
-    archived: {
-        name: 'Archived',
-        technologies: ['typescript', 'next', 'tailwind', 'graphql'],
-        githubURL: 'https://github.com/TAndrewLopez/BlogWebsite',
-        liveURL: '',
-        imageURL: '/image/projects/archived.png',
-        description: "Lightweight Blog Website. ",
-        images: ['/image/projects/archived_splash.png']
-    },
+    // archived: {
+    //     name: 'Archived',
+    //     technologies: ['typescript', 'next', 'tailwind', 'graphql'],
+    //     githubURL: 'https://github.com/TAndrewLopez/BlogWebsite',
+    //     liveURL: '',
+    //     imageURL: '/image/projects/archived.png',
+    //     description: "Lightweight Blog Website. ",
+    //     images: ['/image/projects/archived_splash.png']
+    // },
 }
